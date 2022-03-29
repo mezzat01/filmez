0f4c87bab5d5bc0c6bcf25f061a955ea34425083
mezzat01 <mezzatamine2002@gmail.com>
2022-03-29 17:52:38 +0200

    file init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+// 9fd24c6b 
+const API_URL = 'http://www.omdbapi.com?apikey=9fd24c6b';
 
-function App() {
+
+const App = () => {
+// Creating a function that will fetch our movies .
+const searchMovies = async (title) => {
+      const response = await fetch (`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      console.log(data.Search);
+
+}
+
+
+
+// We will use useEffect to fetch api as soon as our component loads .
+  useEffect(() => {
+     searchMovies('Spiderman');
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React start</h1>
+
     </div>
   );
 }
