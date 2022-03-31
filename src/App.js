97e3fddd3df03835299f7f9c87843d9e0e6f93fa
mezzat01 <mezzatamine2002@gmail.com>
2022-03-31 03:49:17 +0200

    creating movie card component and fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import SearchIcon from './search.svg'
-
+import MovieCard from './MovieCard'
 
 import React from 'react';
 import { useEffect } from 'react';
 // 9fd24c6b 
 const API_URL = 'http://www.omdbapi.com?apikey=9fd24c6b';
+
+
+const movie1 = {
+  "Title": "Italian Spiderman",
+  "Year": "2007",
+  "imdbID": "tt2705436",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+}
+
 
 
 const App = () => {
@@ -25,6 +35,23 @@ const searchMovies = async (title) => {
   return (
     <div className="App">
      <h1>Filmez</h1>
+
+     <div className='search'>
+      <input 
+      placeholder='Search any movie'
+      value="Superman"
+      onChange={() => {}}
+      />
+      <img 
+         src={SearchIcon}
+         alt="search"
+         onClick={() => {}}
+      />
+     </div>
+
+   <div className='container'>
+         <MovieCard movie1={movie1}/>
+   </div>
 
     </div>
   );
