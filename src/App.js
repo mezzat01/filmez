67868bf8ff0f8277164301538e68565b7f0759d4
mezzat01 <mezzatamine2002@gmail.com>
2022-04-02 00:03:35 +0200

    creating a useState for movies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchIcon from './search.svg'
 import MovieCard from './MovieCard'
 
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 // 9fd24c6b 
 const API_URL = 'http://www.omdbapi.com?apikey=9fd24c6b';
 
@@ -21,18 +21,18 @@ const movie1 = {
 
 const App = () => {
 
+const [movies, setMovies] = useState([]);
 
 
 
 
 
-  
 // Creating a function that will fetch our movies .
 const searchMovies = async (title) => {
       const response = await fetch (`${API_URL}&s=${title}`);
       const data = await response.json();
 
-      console.log(data.Search);
+      setMovies(data.Search);
 
 }
 // We will use useEffect to fetch api as soon as our component loads .
